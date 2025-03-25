b6f94c503d063d749c4609c6dbd5eb0ed817aec0
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-03-25 19:08:45 +0200

    add usage of io methods in main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from app.io.input import input_from_console, input_from_file, input_from_file_using_pandas
+from app.io.output import output_to_console, output_to_file, output_to_file_using_pandas
+
+
 def main():
-    pass
+    console_text = input_from_console()
+    file_text = input_from_file("input.txt")
+    pandas_file_text = input_from_file_using_pandas("pandas_input.txt")
+
+    output_to_console(console_text)
+    output_to_console(file_text)
+    output_to_console(pandas_file_text.to_string())
+
+    output_to_file("output.txt", file_text)
+    output_to_file_using_pandas("pandas_output.txt", pandas_file_text)
+
 
 if __name__ == "__main__":
     main()
