1a07796d95b92fb67ecd42cf7951bcdff6a5fadc
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-03-25 19:31:29 +0200

    update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from app.io.output import output_to_console, output_to_file, output_to_file_usin
 
 def main():
     console_text = input_from_console()
-    file_text = input_from_file("input.txt")
-    pandas_file_text = input_from_file_using_pandas("pandas_input.txt")
+    file_text = input_from_file("data/input.txt")
+    pandas_file_text = input_from_file_using_pandas("data/pandas_input.csv")
 
     output_to_console(console_text)
     output_to_console(file_text)
     output_to_console(pandas_file_text.to_string())
 
-    output_to_file("output.txt", file_text)
-    output_to_file_using_pandas("pandas_output.txt", pandas_file_text)
+    output_to_file("data/output.txt", file_text)
+    output_to_file_using_pandas("data/pandas_output.csv", pandas_file_text)
 
 
 if __name__ == "__main__":
