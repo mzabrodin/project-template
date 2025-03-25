15fce2c2e695f661bce49a070d16c884953a5b5a
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-03-25 20:15:12 +0200

    add pytest for input_from_file_using_pandas

--- tests/tests_io/tests_input.py
+++ tests/tests_io/tests_input.py
@@ -9,6 +9,7 @@ def test_input_from_file1():
 
     assert expected == actual
 
+
 def test_input_from_file2():
     test_file = "../../test_data/test_input_from_file2.txt"
     expected = "Testing input_from_file two"
@@ -17,10 +18,38 @@ def test_input_from_file2():
 
     assert expected == actual
 
+
 def test_input_from_file3():
     test_file = "../../test_data/test_input_from_file3.txt"
     expected = "Testing input_from_file |||"
 
     actual = input_from_file(test_file)
+
+    assert expected == actual
+
+
+def test_input_from_file_using_pandas1():
+    test_file = "../../test_data/test_input_from_file_using_pandas1.csv"
+    expected = ['value', '0', 'some_value']
+
+    actual = input_from_file_using_pandas(test_file).to_string().split()
+
+    assert expected == actual
+
+
+def test_input_from_file_using_pandas2():
+    test_file = "../../test_data/test_input_from_file_using_pandas2.csv"
+    expected = ['Name', 'Surname', 'Age', '0', 'Maksym', 'Zabrodin', '19']
+
+    actual = input_from_file_using_pandas(test_file).to_string().split()
+
+    assert expected == actual
+
+
+def test_input_from_file_using_pandas3():
+    test_file = "../../test_data/test_input_from_file_using_pandas3.csv"
+    expected = ['One', 'Two', 'Three', '0', '1', '2', '3', '1', '|', '||', '|||']
+
+    actual = input_from_file_using_pandas(test_file).to_string().split()
 
     assert expected == actual
