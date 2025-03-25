eab6670ab77fbcb82eeb9496f5fc8def05ee0276
Maksym Zabrodin <m.d.zabrodin@gmail.com>
2025-03-25 19:04:08 +0200

    add docstrings for io methods

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,41 @@
 import pandas as pd
 
+
 def input_from_console():
+    """Reads text from console.
+
+    Returns:
+        str: The text read in the console.
+    """
     pass
 
-def input_from_file():
+
+def input_from_file(file_name):
+    """Reads text in file using Python built-in methods.
+
+    Args:
+        file_name (str): The file name argument.
+
+    Returns:
+        str: The text read in the file.
+
+    Raises:
+        FileNotFoundError: If the file with the name ‘file_name’ is not found.
+    """
     pass
 
-def input_from_file_using_pandas():
+
+def input_from_file_using_pandas(csv_file_name):
+    """Reads text in .csv file using Pandas.
+
+    Args:
+        csv_file_name (str): The file name argument.
+
+    Returns:
+        pd.DataFrame: The DataFrame that was read in the .csv file.
+
+    Raises:
+        FileNotFoundError: If the file with the name ‘file_name’ is not found.
+        TypeError: If the file does not have a .csv extension.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,10 +1,37 @@
 import pandas as pd
 
-def output_to_console():
+
+def output_to_console(text):
+    """Prints text to console.
+
+    Args:
+        text (str): The text to print in console.
+    """
     pass
 
-def output_to_file():
+
+def output_to_file(file_name, text):
+    """Writes text to a new .csv file using Python built-in methods.
+
+    Args:
+        file_name (str): The file name argument.
+        text (str): The text to write in file.
+
+    Raises:
+        FileExistsError: If a file with the name ‘file_name’ already exists.
+    """
     pass
 
-def output_to_file_using_pandas():
+
+def output_to_file_using_pandas(file_name, data_frame):
+    """Writes text to a new .csv file using Pandas.
+
+    Args:
+        file_name (str): The file name argument.
+        data_frame (pd.DataFrame): The text to write in file.
+
+    Raises:
+        FileExistsError: If a file with the name ‘file_name’ already exists.
+        TypeError: If the data_frame is not an instance of DataFrame.
+    """
     pass
